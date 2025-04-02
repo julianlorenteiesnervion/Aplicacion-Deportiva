a8ed5c0f1c8b5084d6f507ee6105f9b2f7e50a1b
[julianlorenteiesnervion] <[julian.lorente@iesnervion.es]>
2025-04-02 13:23:26 +0200

    Cambios

--- Equipo.js
+++ Equipo.js
@@ -1,21 +1,31 @@
 class Equipo {
 
-    constructor(nombre, ciudad, estadio) {
+    // Constructor de la clase Equipo que inicializa los atributos del equipo
+    constructor(id, nombre, ciudad, estadio) {
+        this.id = id;
         this.nombre = nombre;
         this.ciudad = ciudad;
         this.estadio = estadio;
     }
 
+    // Devuelve el id del equipo
+    getId() {
+        return this.id;
+    }
+
+    // Devuelve el nombre del equipo
     getNombre() {
         return this.nombre;
     }
 
+    // Devuelve la ciudad del equipo
     getCiudad() {
         return this.ciudad;
     }
 
+    // Devuelve el estadio del equipo
     getEstadio() {
         return this.estadio;
     }
-    
+
 }
--- Modelo.js
+++ Modelo.js
@@ -1,7 +1,64 @@
 class Modelo {
 
     constructor() {
-        
+    }
+
+    // Métodos para manejar jugadores
+    // Obtener todos los jugadores de la lista
+    obtenerJugadores() {
+        return this.listaJugadores;
+    }
+
+    // Agregar un jugador a la lista
+    agregarJugador(jugador) {
+        localStorage.setItem('jugadores', localStorage.getItem('jugadores') + JSON.stringify(jugador));
+    }
+
+    // Eliminar un jugador de la lista mediante su id
+    eliminarJugador(id) {
+        this.listaJugadores = this.listaJugadores.filter(jugador => jugador.getId() !== id);
+    }
+
+    // Actualiza la posición de un jugador mediante su id
+    actualizarPosicionJugador(id, nuevaPosicion) {
+        const jugador = this.listaJugadores.find(jugador => jugador.getId() === id);
+        if (jugador) {
+            jugador.setPosicion(nuevaPosicion);
+        }
+    }
+
+    // Actualiza el equipo de un jugador mediante su id
+    actualizarEquipoJugador(id, nuevoEquipo) {
+        const jugador = this.listaJugadores.find(jugador => jugador.getId() === id);
+        if (jugador) {
+            jugador.setEquipo(nuevoEquipo);
+        }
+    }
+
+    // Obtener los jugadores que pertenecen a X equipo
+    obtenerJugadoresPorEquipo(equipo) {
+        return this.listaJugadores.filter(jugador => jugador.getEquipo() === equipo);
+    }
+
+    // Obtener los jugadores que pertenecen a X posición
+    obtenerJugadoresPorPosicion(posicion) {
+        return this.listaJugadores.filter(jugador => jugador.getPosicion() === posicion);
+    }
+
+    // Métodos para manejar equipos
+    // Obtener todos los equipos de la lista
+    obtenerEquipos() {
+        return this.listaEquipos;
+    }
+
+    // Agregar un equipo a la lista
+    agregarEquipo(equipo) {
+        this.listaEquipos.push(equipo);
+    }
+
+    // Eliminar un equipo de la lista mediante su id
+    eliminarEquipo(id) {
+        this.listaEquipos = this.listaEquipos.filter(equipo => equipo.getId() !== id);
     }
 
 }
