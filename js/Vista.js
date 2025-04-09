ea71d26433477efb112bb024649a3d3aabbbb332
LauraLuzhen <ll.rodriguez@iesnervion.es>
2025-04-09 13:42:51 +0200

    css de laura

--- js/Vista.js
+++ js/Vista.js
@@ -1,49 +1,34 @@
 class Vista {
+    constructor() {
+        this.lista = document.getElementById('lista');
+    }
 
-    mostrarEquipos() {
+    mostrarEquipos(array) {
         let divEquipos = document.createElement('div');
         divEquipos.id = 'listadoEquipos';
 
-        modeloEquipo.obtenerEquipos().forEach(equipo => {
+        array.forEach(equipo => {
             let equipoDiv = document.createElement("div");
             equipoDiv.className = 'infoEquipo';
-            equipoDiv.innerHTML += `${equipo.nombre} <br> Ciudad: ${equipo.ciudad} <br> Estadio: ${equipo.estadio} <br> Número de jugadores en el equipo: `;
-            equipoDiv.innerHTML += `<button id="modificarEquipo">Modificar</button>`;
-            equipoDiv.innerHTML += `<button id="eliminarEquipo">Eliminar</button>`;
-            equipoDiv.innerHTML += `<button id="verJugadores">Ver Jugadores</button>`;
+            equipoDiv.innerHTML += `${equipo.nombre} <br> Ciudad: ${equipo.ciudad} <br> Estadio: ${equipo.estadio} <br> Número de jugadores en el equipo: <button id="modificarEquipo">Modificar</button><button id="eliminarEquipo">Eliminar</button><button id="verJugadores">Ver Jugadores</button>`;
 
             divEquipos.appendChild(equipoDiv);
         });
         document.body.appendChild(divEquipos);
     }
 
-    mostrarJugadores() {
+    mostrarJugadores(array) {
         let divJugadores = document.createElement('div');
         divJugadores.id = 'listadoJugadores';
 
-        modeloEquipo.obtenerJugadores().forEach(equipo => {
+        array.forEach(jugador => {
             let jugadorDiv = document.createElement("div");
             jugadorDiv.className = 'infoJugador';
-            jugadorDiv.innerHTML += `${equipo.nombre} <br> Ciudad: ${equipo.ciudad} <br> Estadio: ${equipo.estadio}`;
-            jugadorDiv.innerHTML += `<button id="modificarEquipo">Modificar</button>`;
-            jugadorDiv.innerHTML += `<button id="eliminarEquipo">Eliminar</button>`;
+            jugadorDiv.innerHTML += `${jugador.nombre} <br> Posición: ${jugador.posicion} <br> Año de nacimiento: ${jugador.annoNacimiento} <br> Equipo: ${jugador.equipo}<button id="modificarJugador">Modificar</button><button id="eliminarJugador">Eliminar</button>`;
 
             divJugadores.appendChild(jugadorDiv);
         });
         document.body.appendChild(divJugadores);
     }
 
-    mostrarPartidos() {
-        let divPartidos = document.createElement('div');
-        divPartidos.id = 'listadoPartidos';
-
-        modeloEquipo.obtenerPartidos().forEach(partido => {
-            let partidoDiv = document.createElement("div");
-            partidoDiv.className = 'infoPartido';
-            partidoDiv.innerHTML += `${partido.puntosEquipo1} ${partido.equipo1} vs ${partido.equipo2} ${partido.puntosEquipo2} <br> Fecha: ${partido.fecha}}`;
-
-            divPartidos.appendChild(partidoDiv);
-        });
-        document.body.appendChild(divPartidos);
-    }
 }
